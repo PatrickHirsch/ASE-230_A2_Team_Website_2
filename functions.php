830e8009b39e80f1b508a1c7de08f8339e53eb02
Patrick Hirsch <HirschP2@nku.edu>
2023-09-07 11:31:25 -0400

    array removed from index.php and details.php and declared in data.php then imported into each via a require_one().  functions.php created and imported in each containing functions previously in details.php and a new set of functions to handle constructing of profile URLs.  Per in-class demonstrations, details.php now stores the specified profile in  reather than calling it from the  array.

---
+++ functions.php
@@ -0,0 +1,34 @@
+<?php
+
+// Given the recurrence of the need to express a range of years, a function is created to do this in a consistent manner.
+// It takes 1-2 values, the first being the start of the range, the second being the end of the range.
+// null is used to represent "Present" or "Still at this position" and is the default value used when no end date is passed.
+function stringifyRange($start, $end = null)
+{
+	if ($end == null)
+		$end = "Present";
+	return $start . " - " . $end;
+}
+
+// Allows the delivery of online profiles as links given a username
+function getProfileLink($username,$platform=null,$protocol=False)
+{	$ret='';
+	if($protocol)	$ret=$ret.'https://';
+	
+	if($platform==null)				$ret=$ret.'';
+	else if($platform=='GitHub')	$ret=$ret.'github.com/';
+	else if($platform=='LinkedIn')	$ret=$ret.'linkedin.com/';
+	
+	$ret=$ret.$username;
+	
+	return $ret;
+}
+// Shorthands for getProfileLink()
+function getGitLink($username,$protocol=False)
+{	return getProfileLink($username,'GitHub',$protocol);
+}
+function getLinkedLink($username,$protocol=False)
+{	return getProfileLink($username,'LinkedIn',$protocol);
+}
+
+?>
